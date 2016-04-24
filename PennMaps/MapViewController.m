6c0749fdce80b76f1a3f08b4ff243d9182e88235
Andrew Fischer <afischer15@mac.com>
2016-04-24 10:13:05 -0400

    Added cocoapods, added geojson overlay with tap recognizer

--- PennMaps/MapViewController.m
+++ PennMaps/MapViewController.m
@@ -9,7 +9,7 @@
 #import "MapViewController.h"
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
-
+#import "GeoJSONSerialization.h"
 
 @interface MapViewController () <CLLocationManagerDelegate, MKMapViewDelegate, UISearchBarDelegate>
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
@@ -24,16 +24,57 @@
     [super viewDidLoad];
     [self  setUpMap];
     self.isUpdatingLocation = NO;
+    
+    [self setStatusBarBackgroundColor];
+    
 }
 
-
+- (void)setStatusBarBackgroundColor {
+    // Jesus there has to be a better way to do this
+    UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+    
+    if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
+        statusBar.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    }
+    statusBar.alpha = 0.75;
+}
 // Set up map to inital state for app
 - (void)setUpMap {
+    self.mapView.delegate = self;
+
     CLLocationCoordinate2D location = CLLocationCoordinate2DMake(39.9520,  -75.1932);
     MKCoordinateRegion viewRegion   = MKCoordinateRegionMakeWithDistance(location, 3000, 0);
     [self.mapView setShowsCompass:NO];
     [self.mapView setRegion:viewRegion];
-    [self.mapView setShowsBuildings:YES];
+    [self.mapView setShowsBuildings:NO];
+    [self.mapView setShowsPointsOfInterest:NO];
+
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleMapTap:)];
+    tap.cancelsTouchesInView = NO;
+    tap.numberOfTapsRequired = 1;
+    
+    UITapGestureRecognizer *tap2 = [[UITapGestureRecognizer alloc] init];
+    tap2.cancelsTouchesInView = NO;
+    tap2.numberOfTapsRequired = 2;
+    
+    [self.mapView addGestureRecognizer:tap2];
+    [self.mapView addGestureRecognizer:tap];
+    [tap requireGestureRecognizerToFail:tap2]; // Ignore single tap if the user actually double taps
+        
+    NSURL *URL = [[NSBundle mainBundle] URLForResource:@"map" withExtension:@"geojson"];
+    NSData *data = [NSData dataWithContentsOfURL:URL];
+    NSDictionary *geoJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    NSArray *shapes = [GeoJSONSerialization shapesFromGeoJSONFeatureCollection:geoJSON error:nil];
+    
+    for (MKShape *shape in shapes) {
+        if ([shape isKindOfClass:[MKPointAnnotation class]]) {
+            [self.mapView addAnnotation:shape];
+        } else if ([shape conformsToProtocol:@protocol(MKOverlay)]) {
+            [self.mapView addOverlay:(id <MKOverlay>)shape];
+        }
+    }
+
 }
 
 - (void)trackUser {
@@ -103,5 +144,58 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id <MKOverlay>)overlay {
+    MKOverlayRenderer *renderer = nil;
+    if ([overlay isKindOfClass:[MKPolyline class]]) {
+        renderer = [[MKPolylineRenderer alloc] initWithPolyline:(MKPolyline *)overlay];
+        ((MKPolylineRenderer *)renderer).strokeColor = [UIColor greenColor];
+        ((MKPolylineRenderer *)renderer).lineWidth = 3.0f;
+    } else if ([overlay isKindOfClass:[MKPolygon class]]) {
+        renderer = [[MKPolygonRenderer alloc] initWithPolygon:(MKPolygon *)overlay];
+//        ((MKPolygonRenderer *)renderer).strokeColor = [UIColor blueColor];
+        ((MKPolygonRenderer *)renderer).fillColor = [UIColor grayColor];
+        ((MKPolygonRenderer *)renderer).lineWidth = 0.0f;
+        
+    }
+    
+    renderer.alpha = 0.5;
+    
+    return renderer;
+}
+
+-(void)handleMapTap:(UIGestureRecognizer*)tap{
+    CGPoint tapPoint = [tap locationInView:self.mapView];
+    
+    CLLocationCoordinate2D tapCoord = [self.mapView convertPoint:tapPoint toCoordinateFromView:self.mapView];
+    MKMapPoint mapPoint = MKMapPointForCoordinate(tapCoord);
+    CGPoint mapPointAsCGP = CGPointMake(mapPoint.x, mapPoint.y);
+    
+    for (id<MKOverlay> overlay in self.mapView.overlays) {
+        if([overlay isKindOfClass:[MKPolygon class]]){
+            MKPolygon *polygon = (MKPolygon*) overlay;
+            
+            CGMutablePathRef mpr = CGPathCreateMutable();
+            
+            MKMapPoint *polygonPoints = polygon.points;
+            
+            for (int p=0; p < polygon.pointCount; p++){
+                MKMapPoint mp = polygonPoints[p];
+                if (p == 0)
+                    CGPathMoveToPoint(mpr, NULL, mp.x, mp.y);
+                else
+                    CGPathAddLineToPoint(mpr, NULL, mp.x, mp.y);
+            }
+            
+            if(CGPathContainsPoint(mpr , NULL, mapPointAsCGP, FALSE)){
+                NSLog(@"Lat: %f, Lon: %f", polygon.coordinate.latitude, polygon.coordinate.longitude);
+                
+            }
+            
+            CGPathRelease(mpr);
+        }
+    }
+}
+
 
 @end
